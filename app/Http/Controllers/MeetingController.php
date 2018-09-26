525b09a137b3e611d9d9c1ab5b0d295deb1238b1
Eneki <joanreneki@gmail.com>
2018-09-26 22:22:47 +0300

    Set up validation for updating meetings. Meetings can only be updated by their creator.

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Meeting;
 use Illuminate\Http\Request;
 use App\Http\Resources\MeetingResource;
+use Illuminate\Support\Facades\Auth;
 
 class MeetingController extends Controller
 {
@@ -26,14 +27,22 @@ class MeetingController extends Controller
         return new MeetingResource($meeting);
     }
 
+    /**
+     * @param Request $request
+     * @param Meeting $meeting
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, Meeting $meeting)
     {
-//      dd($meeting);
-        if ($request->user()->id !== $meeting->creator) {
-            return response()->json(['error' => 'You can only edit meetings you create.'], 403);
+//        dd(Auth::id(), $meeting->creator);
+
+        if (Auth::user() !== $meeting->creator) {
+            return response()->json(['error' => 'You can only edit meetings that you create.'], 403);
         }
 
         $meeting->update($request->all());
+
+        return response()->json(['You have successfully updated your meeting.'], 200);
     }
 
     /**
