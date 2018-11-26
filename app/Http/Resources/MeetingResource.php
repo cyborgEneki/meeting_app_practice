97df437f26035eadce8d56eb8f7b4cf345e20686
Eneki <joanreneki@gmail.com>
2018-11-26 22:56:24 +0300

    Changed meeting from returning objects to returning id

--- app/Http/Resources/MeetingResource.php
+++ app/Http/Resources/MeetingResource.php
@@ -21,15 +21,15 @@ class MeetingResource extends JsonResource
             'date' => $this->date,
             'start_time' => $this->start_time,
             'end_time' => $this->end_time,
-            'creator_id' => new UserResource(User::find($this->creator_id)),
-            'facilitator_id' => new UserResource(User::find($this->facilitator_id)),
-            'time_keeper_id' => new UserResource(User::find($this->time_keeper_id)),
+            'creator_id' => $this->creator_id,
+            'facilitator_id' => $this->facilitator_id,
+            'time_keeper_id' => $this->time_keeper_id,
             'users' => UserResource::collection($this->users),
             'agendas' =>AgendaResource::collection($this->agendas),
-            'venue' => $this->venue,
-            'media' => $this->media,
-            'meetingseries' => $this->meetingseries,
-            'meetingtype' => $this->meetingtype,
+            'venue_id' => $this->venue_id,
+            'media_id' => $this->media_id,
+            'meetingseries_id' => $this->meetingseries_id,
+            'meetingtype_id' => $this->meetingtype_id,
         ];
     }
 }
