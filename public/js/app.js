c88c243db1bda7368f444ff7e727b3c0e8520882
Eneki <joanreneki@gmail.com>
2018-11-29 00:11:27 +0300

    Removed MeetingSeriesList

--- public/js/app.js
+++ public/js/app.js
@@ -55829,126 +55829,13 @@ exports = module.exports = __webpack_require__(1)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
-
-// exports
-
-
-/***/ }),
-/* 116 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__partials_meetingSeries_MeetingSeriesList__ = __webpack_require__(117);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__partials_meetingSeries_MeetingSeriesList___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__partials_meetingSeries_MeetingSeriesList__);
-//
-//
-//
-//
-//
-//
-
-
-/* harmony default export */ __webpack_exports__["default"] = ({
-    name: "MeetingSeriesIndex",
-    props: ['choices'],
-    components: { MeetingSeriesList: __WEBPACK_IMPORTED_MODULE_0__partials_meetingSeries_MeetingSeriesList___default.a }
-});
-
-/***/ }),
-/* 117 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var disposed = false
-function injectStyle (ssrContext) {
-  if (disposed) return
-  __webpack_require__(118)
-}
-var normalizeComponent = __webpack_require__(0)
-/* script */
-var __vue_script__ = __webpack_require__(120)
-/* template */
-var __vue_template__ = __webpack_require__(121)
-/* template functional */
-var __vue_template_functional__ = false
-/* styles */
-var __vue_styles__ = injectStyle
-/* scopeId */
-var __vue_scopeId__ = "data-v-36456034"
-/* moduleIdentifier (server only) */
-var __vue_module_identifier__ = null
-var Component = normalizeComponent(
-  __vue_script__,
-  __vue_template__,
-  __vue_template_functional__,
-  __vue_styles__,
-  __vue_scopeId__,
-  __vue_module_identifier__
-)
-Component.options.__file = "resources/assets/js/components/Meetings/partials/meetingSeries/MeetingSeriesList.vue"
-
-/* hot reload */
-if (false) {(function () {
-  var hotAPI = require("vue-hot-reload-api")
-  hotAPI.install(require("vue"), false)
-  if (!hotAPI.compatible) return
-  module.hot.accept()
-  if (!module.hot.data) {
-    hotAPI.createRecord("data-v-36456034", Component.options)
-  } else {
-    hotAPI.reload("data-v-36456034", Component.options)
-  }
-  module.hot.dispose(function (data) {
-    disposed = true
-  })
-})()}
-
-module.exports = Component.exports
-
-
-/***/ }),
-/* 118 */
-/***/ (function(module, exports, __webpack_require__) {
-
-// style-loader: Adds some css to the DOM by adding a <style> tag
-
-// load the styles
-var content = __webpack_require__(119);
-if(typeof content === 'string') content = [[module.i, content, '']];
-if(content.locals) module.exports = content.locals;
-// add the styles to the DOM
-var update = __webpack_require__(2)("6c0a95ed", content, false, {});
-// Hot Module Replacement
-if(false) {
- // When the styles change, update the <style> tags
- if(!content.locals) {
-   module.hot.accept("!!../../../../../../../node_modules/css-loader/index.js!../../../../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-36456034\",\"scoped\":true,\"hasInlineConfig\":true}!../../../../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./MeetingSeriesList.vue", function() {
-     var newContent = require("!!../../../../../../../node_modules/css-loader/index.js!../../../../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-36456034\",\"scoped\":true,\"hasInlineConfig\":true}!../../../../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./MeetingSeriesList.vue");
-     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-     update(newContent);
-   });
- }
- // When the module is disposed, remove the <style> tags
- module.hot.dispose(function() { update(); });
-}
-
-/***/ }),
-/* 119 */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(1)(false);
-// imports
-
-
-// module
 exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
 
 /***/ }),
-/* 120 */
+/* 116 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -56005,7 +55892,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 });
 
 /***/ }),
-/* 121 */
+/* 117 */,
+/* 118 */,
+/* 119 */,
+/* 120 */,
+/* 121 */,
+/* 122 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var render = function() {
@@ -56085,39 +55977,6 @@ var staticRenderFns = [
     ])
   }
 ]
-render._withStripped = true
-module.exports = { render: render, staticRenderFns: staticRenderFns }
-if (false) {
-  module.hot.accept()
-  if (module.hot.data) {
-    require("vue-hot-reload-api")      .rerender("data-v-36456034", module.exports)
-  }
-}
-
-/***/ }),
-/* 122 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var render = function() {
-  var _vm = this
-  var _h = _vm.$createElement
-  var _c = _vm._self._c || _h
-  return _c(
-    "div",
-    [
-      _c("meeting-series-list", {
-        attrs: { choices: _vm.choices },
-        on: {
-          "update:choices": function($event) {
-            _vm.choices = $event
-          }
-        }
-      })
-    ],
-    1
-  )
-}
-var staticRenderFns = []
 render._withStripped = true
 module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
--- resources/assets/js/components/Meetings/MeetingSeriesIndex.vue
+++ resources/assets/js/components/Meetings/MeetingSeriesIndex.vue
@@ -1,15 +1,50 @@
 <template>
     <div>
-        <meeting-series-list :choices.sync="choices"></meeting-series-list>
+        <router-link :to="{ name: 'addMeetingSeries' }">Add a Type of Meeting Series</router-link>
+        <h3>Meeting Series</h3>
+        <table>
+            <thead>
+            <th>Name</th>
+            <th>Frequency</th>
+            <th>Actions</th>
+            </thead>
+            <tr v-for="eachmeetingseries in orderedMeetingSeries">
+                <td>{{eachmeetingseries.name}}</td>
+                <td>{{eachmeetingseries.frequency}}</td>
+                <td>
+                    <router-link :to="{ name: 'editMeetingSeries', params: { eachmeetingseries } }">Edit</router-link>
+                    <button @click="deleteMeetingSeries(eachmeetingseries.id)">Delete</button>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-    import MeetingSeriesList from "./partials/meetingSeries/MeetingSeriesList";
     export default {
-        name: "MeetingSeriesIndex",
+        name: "MeetingSeriesList",
         props: ['choices'],
-        components: {MeetingSeriesList}
+        data() {
+            return {
+                meetingseries: []
+            }
+        },
+        computed: {
+            orderedMeetingSeries: function () {
+                return _.orderBy(this.choices.meetingseries, 'name')
+            }
+        },
+        methods: {
+            deleteMeetingSeries(id) {
+                axios.delete('/api/meetings/' + id)
+                    .then(() => {
+                        let index = this.meetingseries.map((item) => {
+                            return item.id
+                        }).indexOf(id);
+                        this.meetingseries.splice(index, 1);
+                    })
+            }
+        }
     }
 </script>
 
