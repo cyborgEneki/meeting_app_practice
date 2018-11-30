86aadc12e00694c2cb39fcf44d21af86ce8a12e1
Eneki <joanreneki@gmail.com>
2018-11-30 18:24:54 +0300

    Implemented vue confirmation button

--- resources/assets/js/components/Meetings/UsersIndex.vue
+++ resources/assets/js/components/Meetings/UsersIndex.vue
@@ -20,7 +20,12 @@
                 <td>{{ user.email }}</td>
                 <td>
                     <router-link :to="{ name: 'editUser', params: { user } }">Edit</router-link>
-                    <button @click="deleteUser(user.id)">Delete</button>
+                    <div class="confirmation__button">
+                        <vue-confirmation-button
+                                :messages="customMessages"
+                                v-on:confirmation-success="deleteUser(user.id)">
+                        </vue-confirmation-button>
+                    </div>
                 </td>
             </tr>
             </tbody>
@@ -29,11 +34,22 @@
 </template>
 
 <script>
+    import vueConfirmationButton from 'vue-confirmation-button';
+
     export default {
         name: "UsersList",
+        components: {
+            'vue-confirmation-button': vueConfirmationButton,
+        },
         props: ['choices'],
         data() {
             return {
+                customMessages:
+                    [
+                        'Delete',
+                        'Are you sure?',
+                        'Ok!'
+                    ],
                 users: []
             }
         },
@@ -44,14 +60,6 @@
         },
         methods: {
             deleteUser(id) {
-                this.$dialog.confirm('Please confirm to continue')
-                    .then(function (dialog) {
-                        console.log('Clicked on proceed')
-                    })
-                    .catch(function () {
-                        console.log('Clicked on cancel')
-                    });
-
                 axios.delete('/api/users/' + id)
                     .then(() => {
                         let index = this.users.map((item) => {
