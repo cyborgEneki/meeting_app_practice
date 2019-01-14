897424caef5d7eda78fcd75d592281b0850ff7c2
Eneki <joanreneki@gmail.com>
2019-01-14 20:59:26 +0300

    Frontend set up

--- resources/assets/js/components/Meetings/MeetingSeriesIndex.vue
+++ resources/assets/js/components/Meetings/MeetingSeriesIndex.vue
@@ -1,53 +1,81 @@
 <template>
-    <div>
-        <router-link :to="{ name: 'addMeetingSeries' }">Add a Type of Meeting Series</router-link>
-        <h3>Meeting Series</h3>
-        <table>
-            <thead>
-            <th>Name</th>
-            <th>Frequency</th>
-            <th>Actions</th>
-            </thead>
-            <tr v-for="eachmeetingseries in orderedMeetingSeries">
-                <td>{{eachmeetingseries.name}}</td>
-                <td>{{eachmeetingseries.frequency}}</td>
-                <td>
-                    <router-link :to="{ name: 'editMeetingSeries', params: { eachmeetingseries } }">Edit</router-link>
-                    <button @click="deleteMeetingSeries(eachmeetingseries.id)">Delete</button>
-                </td>
-            </tr>
-        </table>
+  <div>
+    <h2>Meeting Series</h2>
+    <router-link :to="{ name: 'addMeetingSeries' }" class="same-line">
+      <el-button icon="el-icon-circle-plus-outline"></el-button>
+    </router-link>
+
+    <div v-for="eachmeetingseries in orderedMeetingSeries" :key="eachmeetingseries.id" class="meetingdiv">
+      {{eachmeetingseries.name}}<br>
+      {{eachmeetingseries.frequency}}<br>
+      <router-link :to="{ name: 'editMeeting' , params: { meeting }}">
+        <el-button icon="el-icon-edit"></el-button>
+      </router-link>
+      <el-button icon="el-icon-delete same-line" @click.prevent="deleteUser(meeting.id)"></el-button>
     </div>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "MeetingSeriesList",
-        props: ['choices'],
-        data() {
-            return {
-                meetingseries: []
-            }
-        },
-        computed: {
-            orderedMeetingSeries: function () {
-                return _.orderBy(this.choices.meetingseries, 'name')
-            }
-        },
-        methods: {
-            deleteMeetingSeries(id) {
-                axios.delete('/api/meetings/' + id)
-                    .then(() => {
-                        let index = this.meetingseries.map((item) => {
-                            return item.id
-                        }).indexOf(id);
-                        this.meetingseries.splice(index, 1);
-                    })
-            }
-        }
+export default {
+  name: "MeetingSeriesList",
+  props: ["choices"],
+  data() {
+    return {
+      meetingseries: []
+    };
+  },
+  computed: {
+    orderedMeetingSeries: function() {
+      return _.orderBy(this.choices.meetingseries, "name");
     }
+  },
+  methods: {
+    deleteMeetingSeries(id) {
+      axios.delete("/api/meetings/" + id).then(() => {
+        let index = this.meetingseries
+          .map(item => {
+            return item.id;
+          })
+          .indexOf(id);
+        this.meetingseries.splice(index, 1);
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
+.same-line {
+  display: inline-block;
+}
 
+.link {
+  cursor: pointer;
+}
+
+.meetingdiv {
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+  background: #ffffff;
+  position: relative;
+}
+
+.has-search .form-control {
+  padding-left: 2.375rem;
+}
+
+.has-search .form-control-feedback {
+  position: absolute;
+  z-index: 2;
+  display: block;
+  width: 2.375rem;
+  height: 2.375rem;
+  line-height: 2.375rem;
+  text-align: center;
+  pointer-events: none;
+  color: #aaa;
+}
 </style>
